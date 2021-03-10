94f7f97b8c62554abf3730e60f9d6b4c1e5c9dcf
Evgene Popolamov <popolamov908@gmail.com>
2021-03-10 21:40:54 +0300

    fix: change date

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ const dialog = [
       'https://sun2.velcom-by-minsk.userapi.com/c841323/v841323841/6f838/S1UA0Xrscv8.jpg',
     message: 'Привет',
     id: 0,
-    date: '2021-05-02T10:09:04.712Z',
+    date: '2021-03-02T10:09:04.712Z',
     is: 'her',
   },
   {
@@ -12,7 +12,7 @@ const dialog = [
       'https://sun2.velcom-by-minsk.userapi.com/c841323/v841323841/6f838/S1UA0Xrscv8.jpg',
     message: 'Как дела?',
     id: 1,
-    date: '2021-05-02T10:19:04.712Z',
+    date: '2021-03-02T10:19:04.712Z',
     is: 'her',
   },
   {
@@ -20,7 +20,7 @@ const dialog = [
       'https://sun2.velcom-by-minsk.userapi.com/c841323/v841323841/6f838/S1UA0Xrscv8.jpg',
     message: 'Уже пишешь на хуках?',
     id: 2,
-    date: '2021-05-02T10:24:04.712Z',
+    date: '2021-03-02T10:24:04.712Z',
     is: 'her',
   },
   {
@@ -28,7 +28,7 @@ const dialog = [
       'https://sun2.velcom-by-minsk.userapi.com/s/v1/if2/_p9B4qv16SuqH9BzxVlHqII0p5wS2cNXiHUtYOU4mSZ66S2_4mw5IyT7dCHF1HuM0KID_pS-p-F5mfL465Yqa1CX.jpg?size=200x0&quality=96&crop=5,5,1066,1066&ava=1',
     message: 'Привет! Все хорошо',
     id: 3,
-    date: '2021-05-02T11:09:04.712Z',
+    date: '2021-03-02T11:09:04.712Z',
     status: 'sended',
     is: 'my',
   },
@@ -37,7 +37,7 @@ const dialog = [
       'https://sun2.velcom-by-minsk.userapi.com/s/v1/if2/_p9B4qv16SuqH9BzxVlHqII0p5wS2cNXiHUtYOU4mSZ66S2_4mw5IyT7dCHF1HuM0KID_pS-p-F5mfL465Yqa1CX.jpg?size=200x0&quality=96&crop=5,5,1066,1066&ava=1',
     message: 'Да, хуки это круто',
     id: 4,
-    date: '2021-05-02T12:09:04.712Z',
+    date: '2021-03-02T12:09:04.712Z',
     status: 'sended',
     is: 'my',
   },
@@ -46,8 +46,8 @@ const dialog = [
     avatar:
       'https://sun2.velcom-by-minsk.userapi.com/c841323/v841323841/6f838/S1UA0Xrscv8.jpg',
     message: 'Красавчик !',
-    id: 6,
-    date: '2021-05-02T15:09:04.712Z',
+    id: 5,
+    date: '2021-03-02T15:09:04.712Z',
     is: 'her',
   },
 ];
